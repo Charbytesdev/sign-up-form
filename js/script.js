867f7f6ae49e814c31ca1064e1524b189e71f430
Charbytesdev <138222011+Charbytesdev@users.noreply.github.com>
2023-08-16 00:24:32 +0300

    Add password validation with custom errors

--- js/script.js
+++ js/script.js
@@ -4,10 +4,28 @@ const submitButton = document.getElementById("submit-btn");
 const phoneNumber = document.getElementById("phone-number");
 
 function equalsPasswords(password, confirmPassword) {
-  if (password.value === confirmPassword.value) {
+  if (password.value === confirmPassword.value && password.checkValidity()) {
     confirmPassword.setCustomValidity("");
   } else {
     confirmPassword.setCustomValidity("Passwords do not match.");
+  }
+}
+
+function validatePassword(password) {
+  if (password.value.length < 8)
+    password.setCustomValidity("Password must be 8 characters or more.");
+  else if (!password.value.match(/[a-z]/g)) {
+    password.setCustomValidity("Password must include a lowercase Character.");
+  } else if (!password.value.match(/[A-Z]/g)) {
+    password.setCustomValidity("Password must include an UPPERCASE Character.");
+  } else if (!password.value.match(/[0-9]/g)) {
+    password.setCustomValidity("Password must include a Number.");
+  } else if (!password.value.match(/[!@#$%^&*]/g)) {
+    password.setCustomValidity(
+      "Password must include a Special Character(!@#$%^&*)."
+    );
+  } else {
+    password.setCustomValidity("");
   }
 }
 
@@ -21,6 +39,7 @@ confirmPassword.addEventListener("keyup", () =>
   equalsPasswords(password, confirmPassword)
 );
 
-password.addEventListener("keyup", () =>
-  equalsPasswords(password, confirmPassword)
-);
+password.addEventListener("keyup", () => {
+  validatePassword(password);
+  equalsPasswords(password, confirmPassword);
+});
